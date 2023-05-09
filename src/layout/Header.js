f0cd068bab99ff24f2a7ede2a4c31d3f9fccc61c
Oleksii <koliada.oleksii@gmail>
2023-05-09 17:14:22 +0200

    first commit

---
+++ src/layout/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+	return (
+		<header>
+			<nav>
+				<div className="nav-wrapper">
+					<a href="#" className="brand-logo">Logo</a>
+					<ul id="nav-mobile" className="right hide-on-med-and-down">
+						<li><a href="!#">GitHub</a></li>
+
+					</ul>
+				</div>
+			</nav>
+		</header>
+	)
+}
+export { Header }
---
+++ src/layout/Main.js
@@ -0,0 +1,8 @@
+const Main = () => {
+	return (
+		<main className='container content'>
+			
+		</main>
+	)
+}
+export { Main }
